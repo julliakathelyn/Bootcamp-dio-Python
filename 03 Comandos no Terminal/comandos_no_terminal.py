34a30b63843397c96f67b14cf68d87a57374581b
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-08-24 09:40:51 -0300

    Comandos no terminal

---
+++ 03 Comandos no Terminal/comandos_no_terminal.py
@@ -0,0 +1,13 @@
+#comandos no terminal
+
+# se escrevemos python no terminal podemos fazer alguns testes diretamente por la
+# usamos exit() para sair 
+
+# usamos python -i nomedoarquivo.py para fazer o terminal executar um arquivo especifico 
+# usamos exit() para sair 
+
+# dir() é um função que retorna todas as propriedades e metodos de um objeto
+# dir(100) é um função com um argumento dentro que vai retornar o escopo
+
+# help() um função que devolve varias informações quando invocada
+# help(100) um função que também pode tem argumentos de forma parecida com o dir
