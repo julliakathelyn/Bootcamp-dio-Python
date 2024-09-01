6456f9b36e0e45f6d3b3273ede19769126b0d1a8
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-08-31 21:50:05 -0300

    Add files via upload

---
+++ 06 Funçoes de entrada e saida/funcao_entrada_e_saida.py
@@ -0,0 +1,10 @@
+nome = input ("Informa o seu nome: ") #input(telcado)
+print("Seu nome é "+ nome) #print(tela)
+
+nome = "Jullia"
+sobrenome = "Kathelyn"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")# end="...\n" quebra de linha
+print(nome, sobrenome, sep="#") # sep="#" trocar o separador de espaço para outra coisa nesse caso #
+
