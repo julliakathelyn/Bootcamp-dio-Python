14c4170c20ab2f5dcacaa50f7523577c9f31deff
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-09-24 20:43:08 -0300

    Operadores aritméticos

---
+++ 07 Operadores Aritiméticos/operadoresaritimericos.py
@@ -0,0 +1,55 @@
+#adição
+print(1+1)
+
+# Subtração
+print(10-2)
+
+#Multiplicação
+print(4*3)
+
+#Divisão
+print(12/3)
+
+#Divisão inteira
+print(12//2)
+
+#Modulo(resto da  divisão)
+print(10 % 3)
+
+#Expoenciação
+print(2 ** 3)
+
+#Exemploo 
+
+print(10 - 5 * 2)
+print((10 - 5) * 2)
+print(10 ** 2 * 2)
+print(10 ** (2 * 2))
+print(10 / 2 * 4)
+
+print("Adição")
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2)
+
+print("Subtração")
+
+print(produto_1 - produto_2)
+
+print("Divisão")
+
+print(produto_1 / produto_2)
+
+print("Multiplicação")
+
+print(produto_1 * produto_2)
+
+print("Modulo")
+
+print(produto_1 % produto_2)
+
+print("Expoenciação")
+
+print(produto_1 ** produto_2)
