28cb3eb2d2d77dd7ca951f9352bfe270e789f582
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-09-25 22:01:24 -0300

    Add files via upload

---
+++ 08 Operadores de Comparação/operadores_de_comparacao.py
@@ -0,0 +1,22 @@
+#Sempre vai volltar um valor booleano, true se a comparação for verdadeira e false caso não seja
+
+saldo = 450
+saque = 200
+
+# igualdade
+print(saldo == saque) 
+
+# diferença
+print(saldo != saque)
+
+#Maior que 
+print(saldo > saque)
+
+#Maior ou igual
+print(saldo >= saque)
+
+#Menor que
+print(saldo < saque)
+
+#Menor ou igual
+print(saldo <= saque)
