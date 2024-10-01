2d9325da38689f73f55716c8edf864eeaaca2e23
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-09-30 21:12:21 -0300

    arquivo

---
+++ 09 Operadores de Atribuição/operadores_de_atribuicao.py
@@ -0,0 +1,43 @@
+#atribuindo com soma
+
+saldo = 500
+saldo += 200 #recebe
+
+print(saldo)
+
+#atribuindo com subtração
+saldo = 500
+saldo -= 100 #recebe
+
+print(saldo)
+
+#atrubuindo com multiplicação
+saldo = 500
+saldo *= 2 #recebe
+
+print(saldo)
+
+#atribuição com divisão
+
+saldo = 500
+saldo /= 5 #recebe
+
+print(saldo)
+
+saldo = 500
+saldo //=5 #recebe
+
+print(saldo)
+
+#atribuição com módulo
+saldo = 500
+saldo %= 480
+
+print(saldo)
+
+#atribuição com exponenciação
+
+saldo = 80
+saldo **= 2
+
+print(saldo)
