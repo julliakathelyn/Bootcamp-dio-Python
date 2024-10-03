cdfa5f20702e2bf8b6336597277c5b0582f92496
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-03 18:16:04 -0300

    Operadores de associação

---
+++ 12 Operadores de associação/operadores_de_associacao.py
@@ -0,0 +1,12 @@
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+resp = "Python" in curso #esta na sequencia de curso?
+print(resp) 
+
+resp = "maça" not in frutas #não esta na sequencia de frutas?
+print(resp)
+
+resp = 200 in saques #esta na sequencia de saques?
+print(resp)
