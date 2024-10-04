2f604a59b79b2233e19442f1436606f5e17816ce
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-04 13:35:00 -0300

    Blocos

---
+++ 13 Identação e blocos/identacao_e_blocos.py
@@ -0,0 +1,12 @@
+# 4 espaços para separa um bloco de codigo do outro
+
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor sacado")
+        print("retire o seu dinheiro na boca do caixa")
+
+    print("Obrigado por ser nosso cliente")   
+     
+sacar(100)    
