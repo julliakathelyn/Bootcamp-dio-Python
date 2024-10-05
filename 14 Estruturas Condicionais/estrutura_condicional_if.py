df549aa372601148fbe7af7907042e7670d0c25f
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-05 09:07:30 -0300

    If

---
+++ 14 Estruturas Condicionais/estrutura_condicional_if.py
@@ -0,0 +1,9 @@
+saldo = 2000.0
+saque = float(input("Informe o valor do saque: "))
+
+
+if saldo >= saque:
+    print("saque Realizado!") # entra nesse bloco
+
+if saldo < saque:
+    print("Saldo insuficiente") #depois entra nesse bloco
