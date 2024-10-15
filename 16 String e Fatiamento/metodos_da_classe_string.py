48072fa01890cbf26a0e739f84059510faf31bfb
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-15 09:27:09 -0300

    String e Fatiamento

---
+++ 16 String e Fatiamento/metodos_da_classe_string.py
@@ -0,0 +1,24 @@
+curso = "     pYtHon           "
+
+#Maiuscula
+print(curso.upper())
+#minuscula
+print(curso.lower())
+#primeira maiuscula resto minusculo
+print(curso.title())
+
+#remover espaços em branco
+
+print(curso.strip())
+
+print(curso.lstrip())
+
+print(curso.rstrip())
+
+#junções e centralização
+
+curso = "Jullia"
+
+print(curso.center(10, "#"))
+
+print(".".join(curso))
