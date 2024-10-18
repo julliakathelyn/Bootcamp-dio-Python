12ede7cacacea71a7d28eebc2e1466d2e3fafa44
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-18 09:30:51 -0300

    Listas

---
+++ 18 Listas/declarando_listas.py
@@ -0,0 +1,18 @@
+#declarando listas
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+#declatando listas vazia
+frutas = []
+print(frutas)
+
+#vai fazer uma lista que cada letra é um elemento
+letras = list("python")
+print(letras)
+
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
