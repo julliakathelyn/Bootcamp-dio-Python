d2860491a3098c18bfe82c9162e899d077e097ad
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-18 09:30:27 -0300

    Listas

---
+++ 18 Listas/acesso_direto_a_listas.py
@@ -0,0 +1,11 @@
+frutas = ["maçã", "laranja", "uva", "pera"]
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+#indices negativos 
+#podemos usar indices negativos para encontrar itens da lista
+frutas = ["maçã", "laranja", "uva", "pera"]
+
+print(frutas[-1])  # pera
+print(frutas[-3])  # laranja
