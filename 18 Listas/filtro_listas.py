fca0dac6e53172e3264c31d6e818b1a9c3446a61
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-18 09:31:28 -0300

    Listas

---
+++ 18 Listas/filtro_listas.py
@@ -0,0 +1,10 @@
+# Filtrar lista
+numeros = [1, 30, 21, 2, 9, 65, 34]
+#armazena resultados
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Modificar valores
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
