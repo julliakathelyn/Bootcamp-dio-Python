f8621952eb311bc3c2e61ba4d330654ac4760aa8
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-10-20 08:48:25 -0300

    Listas

---
+++ 18 Listas/iterar_listas.py
@@ -0,0 +1,10 @@
+#lista
+
+carros = ["gol", "celta", "palio"]
+
+for carro in carros:
+    print(carro)
+
+#indice = contador carro=item
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
