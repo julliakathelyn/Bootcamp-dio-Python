04800f567d588a991c78caa08136bffa57cf7012
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-11-07 14:58:29 -0300

    count

---
+++ 19 Métodos da classe list/count.py
@@ -0,0 +1,6 @@
+#conta a quantidade de vezes que um obj aparece na lista
+cores = ["vermelho", "azul", "verde", "azul"]
+
+print(cores.count("vermelho"))  # 1
+print(cores.count("azul"))  # 2
+print(cores.count("verde"))  # 1
