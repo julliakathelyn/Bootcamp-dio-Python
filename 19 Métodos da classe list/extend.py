2840d7f309b23e6e72d25a6ad81babcf7d2bff10
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2024-11-07 15:02:05 -0300

    extend

---
+++ 19 Métodos da classe list/extend.py
@@ -0,0 +1,8 @@
+#adiciona igual o apend porem pode adicionar uma lista de uma vez dentro de outra lista
+linguagens = ["python", "js", "c"]
+
+print(linguagens)  # ["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)  # ["python", "js", "c", "java", "csharp"]
